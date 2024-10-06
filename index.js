c86c30f2a08a31b7fca6eb7f25614e9521be9fd5
rishabh <rishu10102002@gmail.com>
2024-10-06 19:46:32 +0530

    connecting to mongodb

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+
+// connect database
+console.log("connecting...");
+mongoose
+    .connect(config.MONGO_URI)
+    .then((res) => console.log("database connected..."))
+    .catch((err) => console.log(err));
+
+const PORT = config.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
